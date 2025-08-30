3de2e67e59e76ea00f9254bdc8396a52056a3971
Aoibu <aoibu.com@gmail.com>
2025-08-30 21:35:39 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://poeakbsfalpyjlzntmun.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZWFrYnNmYWxweWpsem50bXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NzExOTgsImV4cCI6MjA2OTE0NzE5OH0.Xj8-U5s4fP9oIeX7_FOKOdyafo4ktBj-ht-TK9sdasM";
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
